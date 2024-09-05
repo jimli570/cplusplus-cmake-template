7b829f018ad9bc79ec0cf337ce3c35a27935e7bc
Jimmy <jimli570@users.noreply.github.com>
2024-09-05 15:48:54 +0200

    Using std::format

--- src/myApp/source/main.cpp
+++ src/myApp/source/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <format>
 #include <string>
 
 #include <glm/glm.hpp>
@@ -10,16 +11,16 @@
 int main([[maybe_unused]] int argc, [[maybe_unused]] char *argv[] ) {
   mylibrary::MyLibrary mylibrary;
 
-  const glm::vec3 vectorA(1.0f, 0.0f, 0.0f);
+  const glm::vec3 vectorA(1.0f, 0.5f, 0.0f);
   const glm::vec3 vectorB(1.0f, 1.0f, 0.0f);
   const float dotProduct = mylibrary.dotProduct(vectorA, vectorB);
 
   std::cout << "Hello, World!" << std::endl;
 
-  std::cout << "vectorA = (" << vectorA.x << ", " << vectorA.y << ", " << vectorA.z << ")" << std::endl;
-  std::cout << "vectorB = (" << vectorB.x << ", " << vectorB.y << ", " << vectorB.z << ")" << std::endl;
   std::cout << "Using external library for calucating dot product of two vectors: " << std::endl;
-  std::cout << "mylibrary.dotProduct(vectorA, vectorB) = " << dotProduct << std::endl;
+  std::cout << std::format("vectorA = ({}, {}, {})\n", vectorA.x, vectorA.y, vectorA.z);
+  std::cout << std::format("vectorB = ({}, {}, {})\n", vectorB.x, vectorB.y, vectorB.z);
+  std::cout << std::format("mylibrary.dotProduct(vectorA, vectorB) = {}\n", dotProduct);
   
   std::cout << "Your as using version " << myapp::version::VERSION_FULL << " of my 'MyApp'" << std::endl;
   std::cout << "Your as using version " << mylibrary.getVersion() << " of my 'MyLibrary'" << std::endl;
